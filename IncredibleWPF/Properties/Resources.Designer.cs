bf2c87f01773b78b3d00e51a3684e35b0eca74d3
DinarShigapov <dinarhertz@gmail.com>
2022-11-18 21:41:36 +0300

    Add new element

--- IncredibleWPF/Properties/Resources.Designer.cs
+++ IncredibleWPF/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace IncredibleWPF.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap heart {
+            get {
+                object obj = ResourceManager.GetObject("heart", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icon1 {
             get {
                 object obj = ResourceManager.GetObject("icon1", resourceCulture);
